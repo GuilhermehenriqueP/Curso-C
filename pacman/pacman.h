2048501d91971e2ec810d57f4953aa78e867c162
Guilherme <Gui.senai@live.com>
2021-04-05 22:59:30 -0300

    Criando novas funções e quebrando o código em mais arquivos

--- pacman/pacman.h
+++ pacman/pacman.h
@@ -1,18 +1,7 @@
-void liberamapa();
-void lemapa();
-void alocamapa();
 void move(char direcao);
-void imprimemapa();
 int acabou();
 
 
-struct mapa
-{
-    char ** matriz;
-    int linhas;
-    int colunas;
-};
 
-typedef struct mapa MAPA;
 
 
